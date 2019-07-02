ff01a42d621daf6ead060f276df39f8865e38b2b
Cindy Pham <cindy619mysterio@gmail.com>
2019-07-01 23:42:48 -0500

    Created graphic window in main

--- main.cpp
+++ main.cpp
@@ -4,25 +4,49 @@
 
 using namespace std;
 
-int main()
+// Dimensions of window to display
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 512;
+
+
+int main(int argc, char **argv)
 {
     Chip8 myChip;
     
-    // Dimensions of window to display
-    const int SCREEN_WIDTH = 1024;
-    const int SCREEN_HEIGHT = 512;
+    myChip.initialize();
+    
+    // Load the ROM
+    myChip.loadApplication(argv[1]);
 
+    return 0;
+}
+
+void setUpGraphics()
+{
     // Initialize SDL
     SDL_Window* window;
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
         printf("SDL could not be initialized\n");
     
-    // Create window
+    !// Create window
     window = SDL_CreateWindow("CHIP-8 Emulator", 
                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
                         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL)
         printf("Window could not be created\n");
+
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_RendererSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Create texture
+    SDL_Texture* texture = SDL_CreateTexture(renderer,
+                        SDL_PIXELFORMAT_ARGB8888,
+                        SDL_TEXTURE_ACCESS_STREAMING,
+                        64, 32);
+}
+
+void destroyGraphics()
+{
     
-    return 0;
 }
