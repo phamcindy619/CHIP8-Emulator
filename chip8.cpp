e0b4cf7b12a928cb4d044e09b9ebb2eba62dc57f
Cindy Pham <cindy619mysterio@gmail.com>
2019-07-02 02:23:45 -0500

    Finished opcodes and graphics

--- chip8.cpp
+++ chip8.cpp
@@ -1,6 +1,5 @@
 #include "chip8.h"
 #include <fstream>
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -27,29 +26,31 @@ unsigned char chip8_fontset[80] =
      0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
+Chip8::Chip8() {}
+Chip8::~Chip8() {}
+
 void Chip8::initialize()
 {
-    cout << "Initializing...";
-
+    printf("Initializing CHIP-8 Emulator...");
     pc = 0x200;     // App will be loaded at 0x200
     opcode = 0;     // Reset opcode
     I = 0;          // Reset index register
     sp = 0;         // Reset stack pointer
 
     // Clear display screen
-    for (int i = 0; i < GFX_SIZE; i++)
+    for (int i = 0; i < 2048; i++)
         gfx[i] = 0;
 
-    // Clear stack
-    for (int i = 0; i < STACK_SIZE; i++)
+    // Clear stack, keypad, and registers
+    for (int i = 0; i < 16; i++)
+    {
         stack[i] = 0;
-
-    // Clear registers
-    for (int i = 0; i < REG_SIZE; i++)
         V[i] = 0;
+        key[i] = 0;
+    }
 
     // Clear memory
-    for (int i = 0; i < MEM_SIZE; i++)
+    for (int i = 0; i < 4096; i++)
         memory[i] = 0;
 
     // Load font set into memory
@@ -62,12 +63,71 @@ void Chip8::initialize()
 
     srand(time(NULL));
 
-    cout << "Done\n";
+    printf("Done.\n");
 }
 
-bool Chip8::loadApplication(const char* fileName)
+bool Chip8::loadApplication(const char* filename)
 {
+    initialize();
+    printf("Loading ROM: %s...", filename);
+
+    // Open file
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL)
+    {
+        printf("Unable to open file.\n");
+        return false;
+    }
+
+    // Get file size
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    // Allocate memory
+    char* file_buffer = (char*) malloc(sizeof(char) * file_size);
+    if (file_buffer == NULL)
+    {
+        printf("Cannot allocate memory.\n");
+        return false;
+    }
+
+    size_t actual_size = fread(file_buffer, sizeof(char), (size_t) file_size, file);
+    if (actual_size != file_size)
+    {
+        printf("Unable to read file.\n");
+        return false;
+    }
+
+    // Copy buffer to memory
+    if ((4096 - 512) > file_size)
+    {
+        // Start at 0x200
+        for (int i = 0; i < file_size; i++)
+            memory[i + 512] = (uint8_t) file_buffer[i];
+    }
+    else
+    {
+        printf("File too large.\n");
+        return false;
+    }
+
+    // Close file
+    fclose(file);
+    free(file_buffer);
+
+    printf("Done.\n");
     return true;
+}
+
+void Chip8::setDraw(bool flag)
+{
+    drawFlag = flag;
+}
+
+bool Chip8::getDraw()
+{
+    return drawFlag;
 }
 
 // Fetch the opcode
@@ -82,18 +142,20 @@ void Chip8::decode()
     switch (opcode & 0xF000)
     {
         case 0x0000:
-            switch (opcode & 0xFF)
+            switch (opcode & 0xF)
             {
                 // 00E0: Clear screen
-                case 0xE0:
-                    for (int i = 0; i < GFX_SIZE; i++)
+                case 0x0:
+                    for (int i = 0; i < 2048; i++)
                         gfx[i] = 0;
+                    drawFlag = true;
                     pc += 2;
                     break;
                 // 00EE: Return from function
-                case 0xEE:
-                    pc = stack[sp];
+                case 0xE:
                     sp--;
+                    pc = stack[sp];
+                    pc += 2;
                     break;
                 default:
                     printf("Unknown opcode: 0x%X\n", opcode);
@@ -101,13 +163,13 @@ void Chip8::decode()
             break;
         // 1NNN: Goto address NNN
         case 0x1000:
-            pc = opcode & 0x0FFF;
+            pc = opcode & 0xFFF;
             break;
         // 2NNN: Call function at address NNN
         case 0x2000:
             stack[sp] = pc;
             sp++;
-            pc = opcode & 0x0FFF;
+            pc = opcode & 0xFFF;
             break;
         // 3XNN: If V[X] == NN, skip next instruction
         case 0x3000:
@@ -117,7 +179,7 @@ void Chip8::decode()
             break;
         // 4XNN: If V[X] != NN, skip next instruction
         case 0x4000:
-            if (V[(opcode & 0xF00) >> 8] != 0xFF)
+            if (V[(opcode & 0xF00) >> 8] != (opcode & 0xFF))
                 pc += 2;
             pc += 2;
             break;
@@ -227,13 +289,36 @@ void Chip8::decode()
             break;
         // DXYN: Draw sprite at (V[X], V[Y]) with width of 8 px and height of N px
         case 0xD000:
+        {
+            unsigned short x = V[(opcode & 0xF00) >> 8];
+            unsigned short y = V[(opcode & 0xF0) >> 4];
+            unsigned short height = opcode & 0xF;
+            unsigned short px;
+
+            V[0xF] = 0;
+            for (int yLine = 0; yLine < height; yLine++)
+            {
+                px = memory[I + yLine];
+                for (int xLine = 0; xLine < 8; xLine++)
+                {
+                    if ((px & (0x80 >> xLine)) != 0)
+                    {
+                        if (gfx[(x + xLine + ((y + yLine) * 64))] == 1)
+                            V[0xF] = 1;
+                        gfx[x + xLine + ((y + yLine) * 64)] ^= 1;
+                    }    
+                }
+            }
+            drawFlag = true;
+            pc += 2;
             break;
+        }
         case 0xE000:
             switch (opcode & 0xFF)
             {
                 // EX9E: If key stored in V[X] is pressed, skip next instruction
                 case 0x9E:
-                    if (key[V[(opcode & 0xF00) >> 8]] == 1)
+                    if (key[V[(opcode & 0xF00) >> 8]] != 1)
                         pc += 2;
                     pc += 2;
                     break;
@@ -257,7 +342,23 @@ void Chip8::decode()
                     break;
                 // FX0A: Wait for key press and then store in V[X]
                 case 0x0A:
+                {
+                    bool key_pressed = false;
+                    for (int i = 0; i < 16; i++)
+                    {
+                        // Key is pressed
+                        if (key[i] != 0)
+                        {
+                            V[(opcode & 0xF00) >> 8] = i;
+                            key_pressed = true;
+                        }
+                    }
+
+                    if (!key_pressed)
+                        return;
+                    pc += 2;
                     break;
+                }
                 // FX15: Set delay timer to V[X]
                 case 0x15:
                     delay_timer = V[(opcode & 0xF00) >> 8];
@@ -270,12 +371,18 @@ void Chip8::decode()
                     break;
                 // FX1E: Add V[X] to I
                 case 0x1E:
+                    // There is overflow
+                    if (I + V[(opcode & 0xF00) >> 8] > 0xFFF)
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
                     I += V[(opcode & 0xF00) >> 8];
                     pc += 2;
                     break;
                 // FX29: Set I to location of sprite for character in V[X]
                 case 0x29:
-
+                    I = V[(opcode & 0xF00) >> 8] * 0x5;
+                    pc += 2;
                     break;
                 // FX33: Store BCD representation of V[X] at address I, I + 1, I + 2
                 case 0x33:
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "chip8.h"
 #include <SDL2/SDL.h>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -8,27 +10,38 @@ using namespace std;
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 512;
 
+// Keypad map
+uint8_t keyMap[16] = {
+    SDLK_x,
+    SDLK_1,
+    SDLK_2,
+    SDLK_3,
+    SDLK_q,
+    SDLK_w,
+    SDLK_e,
+    SDLK_a,
+    SDLK_s,
+    SDLK_d,
+    SDLK_z,
+    SDLK_c,
+    SDLK_4,
+    SDLK_r,
+    SDLK_f,
+    SDLK_v
+};
+
 
 int main(int argc, char **argv)
 {
-    Chip8 myChip;
-    
-    myChip.initialize();
-    
-    // Load the ROM
-    myChip.loadApplication(argv[1]);
+    Chip8 myChip = Chip8();
+    bool quit = false;
 
-    return 0;
-}
-
-void setUpGraphics()
-{
     // Initialize SDL
     SDL_Window* window;
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
         printf("SDL could not be initialized\n");
     
-    !// Create window
+    // Create window
     window = SDL_CreateWindow("CHIP-8 Emulator", 
                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
                         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
@@ -37,16 +50,85 @@ void setUpGraphics()
 
     // Create renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_RendererSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // Create texture
     SDL_Texture* texture = SDL_CreateTexture(renderer,
                         SDL_PIXELFORMAT_ARGB8888,
-                        SDL_TEXTURE_ACCESS_STREAMING,
+                        SDL_TEXTUREACCESS_STREAMING,
                         64, 32);
-}
-
-void destroyGraphics()
-{
     
+    // Temp buffer
+    uint32_t pixels[2048];
+
+    myChip.initialize();
+    
+    // Load the ROM
+    myChip.loadApplication(argv[1]);
+
+    // Program loop
+    while (!quit)
+    {
+        myChip.emulateCycle();
+
+        // Process events
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            // Quit emulator
+            if (event.type == SDL_QUIT)
+                quit = true;
+            else if (event.type == SDL_KEYDOWN)
+            {
+                // Quit emulator
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    quit = true;
+                // Reload ROM
+                else if (event.key.keysym.sym == SDLK_F1)
+                    myChip.loadApplication(argv[1]);
+
+                // Store key state as pressed
+                for (int i = 0; i < 16; i++)
+                {
+                    if (event.key.keysym.sym == keyMap[i])
+                        myChip.key[i] = 1;
+                }
+            }
+            else if (event.type = SDL_KEYUP)
+            {
+                // Store key state as unpressed
+                for (int i = 0; i < 16; i++)
+                {
+                    if (event.key.keysym.sym == keyMap[i])
+                        myChip.key[i] = 0;
+                }
+                
+            }
+        }
+
+        // Redraw SDL screen if draw flag is true
+        if (myChip.getDraw())
+        {
+            printf("Drawing...\n");
+            // Store px in temp buffer
+            for (int i = 0; i < 2048; i++)
+            {
+                uint8_t px = myChip.gfx[i];
+                pixels[i] = (0xFFFFFF * px) | 0xFF000000;
+            }
+
+            // Update texture
+            SDL_UpdateTexture(texture, NULL, pixels, 64 * sizeof(uint32_t));
+        
+            // Clear and render
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
+
+            // Reset draw flag
+            myChip.setDraw(false);
+        }
+
+        std::this_thread::sleep_for(std::chrono::microseconds(1200));
+    }
 }
